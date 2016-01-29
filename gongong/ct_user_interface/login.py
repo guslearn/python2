47114b21c82ccee66e17449e120c7ec4194f325e
gbj_ct <guslearn@126.com>
2016-01-29 16:55:34 +0800

    20160129 commit

--- gongong/ct_user_interface/login.py
+++ gongong/ct_user_interface/login.py
@@ -6,6 +6,7 @@ import cookielib
 import data
 import register
 
+'''
 url_login = data.url_login
 cookie = cookielib.CookieJar()
 
@@ -56,4 +57,12 @@ response1 = opener.open(req1)
 response_read1 = response1.read()
 #res1 = json.loads(response_read1)
 print response_read1
+'''
+
+testurl = "http://usersvc.ct108.org:1904/test/GetRegSmsCode.aspx?mobile=13112340204"
+req3 = urllib2.Request(testurl)
+response3 = urllib2.urlopen(req3)
+print response3.read()
+
+
 
