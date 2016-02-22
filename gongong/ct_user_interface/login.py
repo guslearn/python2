d3cc2aa41b825a2757621e23580e1c82fecd15a0
gbj_ct <guslearn@126.com>
2016-02-22 19:33:57 +0800

    修改登录方法-新增手机密码登录（未完成）

--- gongong/ct_user_interface/login.py
+++ gongong/ct_user_interface/login.py
@@ -23,6 +23,7 @@ class Login:
     _userpwd = password
     _cookie = ""
     _hardinfo = mobilehard
+    _mobile = ""
     #默认为同设备用户名密码登录
     def __init__(self):
         self.login_by_username_same_hard()
@@ -68,7 +69,7 @@ class Login:
         else:
             print "Not Sucessed!"
 
-    #用户名密码登录
+    #不同设备上用户名密码登录
     def login_by_username(self):
         __cookie = cookielib.CookieJar()
         value1 = {"UserName":self._username,"Password":password,"AppId":66,"RecommenderID":11,"MobileHardInfo":{"WifiID":"745c39fa0901","SystemID":"34b4206771049e8","ImeiID":"162517026242371","ImsiID":"","SimSerialNO":""},"DownloadGroup":6,"StatExtInfo":{"FromAppId":66},"CodeID":"A5E31C95EE4F409CB75D4C8262420454","VerifyCode":"7ab1","Version ":"20150821"}
@@ -87,3 +88,30 @@ class Login:
         else:
             print "Not Sucessed!"
 
+
+    def login_by_mobile_and_pwd(self):
+        __cookie = cookielib.CookieJar()
+        self._mobile = reg1._mobile
+        value1 = {"Mobile":self._username,"Password":password,"AppId":66}
+
+        handler = urllib2.HTTPCookieProcessor(__cookie)
+        opener = urllib2.build_opener(handler)
+        #urllib2.install_opener(opener)
+
+        datas = json.dumps(value1)
+        req = urllib2.Request(url_login,datas, {'Content-Type': 'application/json'})
+
+        response = opener.open(req)
+        #response = urllib2.urlopen(req)
+        response_read = response.read()
+
+        res = json.loads(response_read)
+        #print res["StatusCode"]
+        #print cookie
+        if res["StatusCode"] == 0:
+            #return __cookie
+            self._cookie = __cookie
+            print self._username
+            print self._userpwd
+        else:
+            print "Not Sucessed!"
